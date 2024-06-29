66f3e210b7f15e8b40786900ef6fe61a0e05b592
saeedmar <saeed.maruffy@hotmail.com>
2024-06-30 00:57:58 +0200

    eerste-en-tweede-opdrachten-zijn-klaar

--- functions-methods.js
+++ functions-methods.js
@@ -3,25 +3,50 @@
 
 
 /* Opdracht  1 */
-// Schrijf een functie genaamd getEmailDomain, die een emailadres verwacht en de domeinnaam teruggeeft. Een domeinnaam is hetgeen dat na het @ in het adres staat
+// Schrijf een functie genaamd getEmailDomain, die een emailadres verwacht en de domeinnaam teruggeeft.
+// Een domeinnaam is hetgeen dat na het @ in het adres staat
 // ---- Verwachte uitkomsten:
 // getEmailDomain("n.eeken@novi-education.nl") geeft novi-education.nl
 // getEmailDomain("t.mellink@novi.nl") geeft novi.nl
 // getEmailDomain("a.wiersma@outlook.com") geeft outlook.com
 
-
+// function getEmailDomain1 (email){
+// const positionNumber = email.indexOf('@')
+//     const emailDomain = email.substring(positionNumber + 1);
+// return emailDomain;
+// }
+// console.log(getEmailDomain1('hello@gmail.com'));
 
 
 /* Opdracht  2 */
-// Schrijf een functie genaamd typeOfEmail, die een emailadres verwacht. De functie checkt of het emailadres een novi domein heeft (medewerker), een novi-education domein (student), of extern domein (zoals gmail of outlook)
+// Schrijf een functie genaamd typeOfEmail, die een emailadres verwacht.
+// De functie checkt of het emailadres een novi domein heeft (medewerker),
+// e
+// en novi-education domein (student), of extern domein (zoals gmail of outlook)
 // ---- Verwachte uitkomsten:
 // typeOfEmail("n.eeken@novi-education.nl") geeft "Student"
 // typeOfEmail("t.mellink@novi.nl") geeft geeft "Medewerker"
 // typeOfEmail("novi.nlaapjesk@outlook.com") geeft geeft "Extern" <-- deze moet het ook doen!
 // typeOfEmail("a.wiersma@outlook.com") geeft "Extern"
 
+function typeOfEmail (email){
+  const domain = getEmailDomain(email);
+  if (domain === "novi-education.nl"){
+      return "Student";
+  }
+  else if (domain === "novi.nl"){
+      return "Medewerker"
+  }
+    else {
+        return "Extern";
+  }
+}
+function getEmailDomain (email){
+    const positionOfAt = email.indexOf('@');
+    return email.substring(positionOfAt + 1);
+}
 
-
+console.log(typeOfEmail('a.wiersma@novi.nl'));
 /* Opdracht  3 */
 // Schrijf een functie genaamd checkEmailValidity, die een emailadres verwacht en checkt of het emailadres valide is. De functie returned true of false, afhankelijk van de uitkomst.
 // Een emailadres is valide wanneer:
